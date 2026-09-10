d96b4861ab9a31e71f374313353e01070975276e
DESKTOP-PK40RMJ\Marija <marija.kristic@gmail.com>
2026-09-10 02:16:38 +0200

    add server parameters

--- src/interface/app_v2.py
+++ src/interface/app_v2.py
@@ -354,4 +354,11 @@ with gr.Blocks(title="Croatian Query2Box") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(css=css, theme=theme)
+    demo.launch(
+        server_name="127.0.0.1",
+        server_port=7860,
+        share=False,
+        inbrowser=False,
+        css=css,
+        theme=theme,
+    )
